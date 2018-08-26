af8de3796c14ecaff9adb08486cffe732c96dccd
Rener Baffa da Silva <renerbaffa@reners-mbp.speedport.ip>
2018-08-26 19:15:23 +0200

    Adding test for success

--- src/App.test.js
+++ src/App.test.js
@@ -4,34 +4,22 @@ import App from './App';
 
 import fetch from 'fetch-mock'
 
-test('wat', () => {
-  const container = document.createElement('div');
-  function App() {
-    setTimeout(() => {
-      throw new Error('hi');
-    }, 100)
-    return null
-  }
-  expect(() => {
-    ReactDOM.render(<App />, container);
-  }).toThrow();
+
+it('renders without crashing', async () => {
+  fetch.once('*', { data: '12345' })
+  
+  const {getByText, queryByText} = render(<App />);
+  expect(getByText('Loading')).toBeInTheDocument()
+  await wait(() => expect(queryByText('Loading')).not.toBeInTheDocument())
+  await wait(() => expect(queryByText('Loading')).toBeNull())
 });
 
+// need to find out why the test is breaking
+it.skip('crashing', async () => {
+  fetch.once('*', { data: '12345' })
 
-// it('renders without crashing', async () => {
-//   fetch.once('*', { data: '12345' })
-  
-//   const {getByText, queryByText} = render(<App />);
-//   expect(getByText('Loading')).toBeInTheDocument()
-//   // await wait(() => expect(queryByText('Loading')).not.toBeInTheDocument())
-//   await wait(() => expect(queryByText('Loading')).toBeNull())
-// });
-
-// it.skip('crashing', async () => {
-//   fetch.once('*', { data: '12345' })
-
-//   const {getByText} = render(<App />);
-//   expect(getByText('Loading')).toBeInTheDocument()
-//   // await wait(() => expect(queryByText('Loading')).not.toBeInTheDocument())
-//   await wait(() => expect(getByText('Error!')).toBeInTheDocument())
-// });
+  const {getByText} = render(<App />);
+  expect(getByText('Loading')).toBeInTheDocument()
+  // await wait(() => expect(queryByText('Loading')).not.toBeInTheDocument())
+  await wait(() => expect(getByText('Error!')).toBeInTheDocument())
+});
