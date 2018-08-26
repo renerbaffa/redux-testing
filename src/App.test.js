abc0ec46916a890e4aa85d4699b031edb913f7e4
Rener Baffa da Silva <renerbaffa@reners-mbp.speedport.ip>
2018-08-26 21:33:06 +0200

    Testing rejecting promise by simulating error on fetch

--- src/App.test.js
+++ src/App.test.js
@@ -4,22 +4,25 @@ import App from './App';
 
 import fetch from 'fetch-mock'
 
+describe('Landing page tests', () => {
+  afterEach(() => {
+    fetch.restore()
+  })
 
-it('renders without crashing', async () => {
-  fetch.once('*', { data: '12345' })
+  it('renders without crashing', async () => {
+    fetch.once('*', { data: '12345' })
+    
+    const {getByText, queryByText} = render(<App />);
+    expect(getByText('Loading')).toBeInTheDocument()
+    await wait(() => expect(queryByText('Loading')).not.toBeInTheDocument())
+  });
   
-  const {getByText, queryByText} = render(<App />);
-  expect(getByText('Loading')).toBeInTheDocument()
-  await wait(() => expect(queryByText('Loading')).not.toBeInTheDocument())
-  await wait(() => expect(queryByText('Loading')).toBeNull())
-});
-
-// need to find out why the test is breaking
-it.skip('crashing', async () => {
-  fetch.once('*', { data: '12345' })
-
-  const {getByText} = render(<App />);
-  expect(getByText('Loading')).toBeInTheDocument()
-  // await wait(() => expect(queryByText('Loading')).not.toBeInTheDocument())
-  await wait(() => expect(getByText('Error!')).toBeInTheDocument())
-});
+  // need to find out why the test is breaking
+  it('crashing', async () => {
+    fetch.once('*', {status: 500})
+  
+    const {getByText, queryByText} = render(<App />);
+    expect(getByText('Loading')).toBeInTheDocument()
+    await wait(() => expect(getByText('Error!')).toBeInTheDocument())
+  });
+})
